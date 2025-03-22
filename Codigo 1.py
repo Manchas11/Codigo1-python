1bc6593e5e47424ce082608909e877391ce15e4f
Manchas11 <202404065@utacapulco.edu.mx>
2025-03-21 22:35:51 -0600

    Add files via upload

---
+++ Codigo 1.py
@@ -0,0 +1,9 @@
+pi = 3.1416
+
+radio = float(input("Intruduce el radio del circulo: "))
+
+area = pi*(radio**2)
+per = 2*radio*pi
+
+print("El area del circulo es: ",area)
+print("El perimetro del circulo es:",per)
